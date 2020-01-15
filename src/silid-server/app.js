1190699a0a54b07388fcd1c89bed967378265dec
Daniel Bidulock <daniel_bidulock@sil.org>
2020-01-15 16:11:55 -0700

    Sketch-in passport-auth0 strategy stuff

--- src/silid-server/app.js
+++ src/silid-server/app.js
@@ -1,19 +1,23 @@
 require('dotenv-flow').config();
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const logger = require('morgan');
 const cors = require('cors');
-const serverless = require("serverless-http");
+//const serverless = require("serverless-http");
 
-var indexRouter = require('./routes/index');
-var agentRouter = require('./routes/agent');
-var organizationRouter = require('./routes/organization');
-var teamRouter = require('./routes/team');
 
-const jwt = require('express-jwt');
-const jwksRsa = require('jwks-rsa');
+const authRouter = require('./routes/auth');
+const indexRouter = require('./routes/index');
+const agentRouter = require('./routes/agent');
+const organizationRouter = require('./routes/organization');
+const teamRouter = require('./routes/team');
+
+//const jwt = require('express-jwt');
+//const jwksRsa = require('jwks-rsa');
 
 
 var app = express();
@@ -21,7 +25,7 @@ var app = express();
 /**
  * SPA client route
  */
-app.use('/', indexRouter);
+//app.use('/', indexRouter);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -32,6 +36,13 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(session({
+  secret: process.env.AUTH0_CLIENT_SECRET, // This seemed convenient
+  resave: true,
+  //cookie: { secure: false},
+  saveUninitialized: true
+}));
 
 if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
   app.use(express.static(path.join(__dirname, 'build')));
@@ -41,28 +52,64 @@ else {
 }
 
 /**
- * Access Token verification
+ * passport-auth0
  */
-const protocol = process.env.NODE_ENV === 'test' || process.env.NODE_ENV === 'e2e' ? 'http' : 'https';
-const checkJwt = jwt({
-  secret: jwksRsa.expressJwtSecret({
-    cache: true,
-    rateLimit: true,
-    jwksRequestsPerMinute: 5,
-    jwksUri: `${protocol}://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`
-  }),
+const Auth0Strategy = require('passport-auth0');
+const passport = require('passport');
 
-  audience: process.env.AUTH0_AUDIENCE,
-  issuer: `${protocol}://${process.env.AUTH0_DOMAIN}/`,
-  requestProperty: 'agent',
-  algorithm: ['RS256']
+const strategy = new Auth0Strategy({
+   domain:       process.env.AUTH0_DOMAIN,
+   clientID:     process.env.AUTH0_CLIENT_ID,
+   clientSecret: process.env.AUTH0_CLIENT_SECRET,
+   callbackURL:  '/callback'
+  },
+  function(accessToken, refreshToken, extraParams, profile, done) {
+    // accessToken is the token to call Auth0 API (not needed in the most cases)
+    // extraParams.id_token has the JSON Web Token
+    // profile has all the information from the user
+    return done(null, profile);
+  }
+);
+
+passport.use(strategy);
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
 });
 
-app.use(checkJwt);
+passport.deserializeUser(function(id, done) {
+  done(null, user);
+});
+
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+/**
+ * Access Token verification
+ */
+//const protocol = process.env.NODE_ENV === 'test' || process.env.NODE_ENV === 'e2e' ? 'http' : 'https';
+//const checkJwt = jwt({
+//  secret: jwksRsa.expressJwtSecret({
+//    cache: true,
+//    rateLimit: true,
+//    jwksRequestsPerMinute: 5,
+//    jwksUri: `${protocol}://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`
+//  }),
+//
+//  audience: process.env.AUTH0_AUDIENCE,
+//  issuer: `${protocol}://${process.env.AUTH0_DOMAIN}/`,
+//  requestProperty: 'agent',
+//  algorithm: ['RS256']
+//});
+//
+//app.use(checkJwt);
 
 /**
  * Routes
  */
+app.use('/', authRouter);
 app.use('/agent', agentRouter);
 app.use('/organization', organizationRouter);
 app.use('/team', teamRouter);
