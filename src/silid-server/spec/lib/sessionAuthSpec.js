4adfa85c71a2608ef8fff971c35e135c4c7ff0e9
Daniel Bidulock <daniel_bidulock@sil.org>
2020-01-16 10:36:26 -0700

    Hello, sessionAuthSpec

---
+++ src/silid-server/spec/lib/sessionAuthSpec.js
@@ -0,0 +1,156 @@
+'use strict';
+require('dotenv').config();
+
+const nock = require('nock');
+const httpMocks = require('node-mocks-http');
+const fixtures = require('sequelize-fixtures');
+const models = require('../../models');
+const Agent = models.Agent;
+
+const sessionAuth = require('../../lib/sessionAuth');
+
+/**
+ * 2019-11-13
+ * Sample tokens taken from:
+ *
+ * https://auth0.com/docs/api-auth/tutorials/adoption/api-tokens
+ */
+const _identity = require('../fixtures/sample-auth0-identity-token');
+
+describe('sessionAuth', function() {
+
+  let agent, request, response;
+
+  describe('returning visitor', () => {
+
+    let authenticatedSession;
+    beforeEach(function(done) {
+      response = httpMocks.createResponse();
+
+      models.sequelize.sync({force: true}).then(() => {
+        fixtures.loadFile(`${__dirname}/../fixtures/agents.json`, models).then(() => {
+          models.Agent.findAll().then(results => {
+            agent = results[0];
+            done();
+          }).catch(err => {
+            done.fail(err);
+          });
+        }).catch(err => {
+          done.fail(err);
+        });
+      }).catch(err => {
+        done.fail(err);
+      });
+    });
+
+    it('attaches agent to request object', done => {
+      request = httpMocks.createRequest({
+        method: 'POST',
+        url: '/agent',
+        user: _identity
+      });
+
+      sessionAuth(request, response, function(err) {
+        if (err) return done.fail(err);
+        expect(request.agent.email).toEqual(agent.email);
+        done();
+      });
+    });
+
+    it('saves the Identity Token in the agent\'s socialProfile', done => {
+      request = httpMocks.createRequest({
+        method: 'POST',
+        url: '/agent',
+        user: _identity
+      });
+
+      expect(agent.socialProfile).toBeNull();
+
+      sessionAuth(request, response, function(err) {
+        if (err) return done.fail(err);
+
+        models.Agent.findOne({ where: { email: _identity.email }}).then(agent => {
+          expect(agent.socialProfile).toEqual(_identity);
+          done();
+
+        }).catch(err => {
+          done.fail(err);
+        });
+      });
+    });
+  });
+
+  describe('first visit', () => {
+    beforeEach(function(done) {
+      response = httpMocks.createResponse();
+  
+      models.sequelize.sync({force: true}).then(() => {
+        done();
+      }).catch(err => {
+        done.fail(err);
+      });
+    });
+
+    it('attaches agent to request object', done => {
+      request = httpMocks.createRequest({
+        method: 'POST',
+        url: '/agent',
+        user: _identity
+      });
+
+      sessionAuth(request, response, function(err) {
+        if (err) return done.fail(err);
+        Agent.findOne({ where: { id: agent.id } }).then(a => {
+          expect(request.agent.dataValues).toEqual(a.dataValues);
+          done();
+        }).catch(err => {
+          done.fail();
+        });
+      });
+    });
+
+    it('adds agent to the database', done => {
+      request = httpMocks.createRequest({
+        method: 'POST',
+        url: '/agent',
+        user: _identity
+      });
+
+      Agent.findAll().then(a => {
+        expect(a.length).toEqual(0);
+
+        sessionAuth(request, response, function(err) {
+          if (err) return done.fail(err);
+          Agent.findOne({ where: { email: _identity.email } }).then(a => {
+            expect(request.agent.dataValues).toEqual(a.dataValues);
+            done();
+          }).catch(err => {
+            done.fail(err);
+          });
+        });
+
+      }).catch(err => {
+        done.fail();
+      });
+    });
+
+    it('saves the Identity Token in the agent\'s socialProfile', done => {
+      request = httpMocks.createRequest({
+        method: 'POST',
+        url: '/agent',
+        user: _identity
+      });
+
+      sessionAuth(request, response, function(err) {
+        if (err) return done.fail(err);
+        expect(request.agent.socialProfile).toEqual(_identity);
+        Agent.findOne({ where: { email: request.agent.email } }).then(a => {
+          expect(request.agent.socialProfile).toEqual(a.socialProfile);
+          done();
+        }).catch(err => {
+          done.fail(err);
+        });
+      });
+    });
+  });
+});
