7769dca7eb1a3682125edac15f39c5c60fe6ed26
Daniel Bidulock <daniel_bidulock@sil.org>
2020-01-23 09:52:37 -0700

    Get reasonable working draught of the mock Auth0 server

--- src/silid-server/spec/support/mockAuth0Server.js
+++ src/silid-server/spec/support/mockAuth0Server.js
@@ -1,9 +1,17 @@
-'use strict';
+/**
+ * Mock Auth0 server for end-to-end tests.
+ *
+ * Trick the client and server into thinking they're actually talking to Auth0
+ *
+ * This server satisifies `authorization_code` server-side session
+ * authentication (probably)
+ */
 require('dotenv-flow').config();
 
 const setupKeystore = require('../support/setupKeystore');
 const jwt = require('jsonwebtoken');
 const pem = require('pem');
+const crypto = require('crypto');
 
 
 if (process.env.NODE_ENV === 'e2e') {
@@ -22,168 +30,190 @@ if (process.env.NODE_ENV === 'e2e') {
  *
  * https://auth0.com/docs/api-auth/tutorials/adoption/api-tokens
  */
-//const _access = require('../fixtures/sample-auth0-access-token');
-//_access.iss = `http://${process.env.AUTH0_DOMAIN}/`;
 const _identity = require('../fixtures/sample-auth0-identity-token');
 
 /**
- * Allows multiple identities
+ * The agent ID token "database"
  */
 const identityDb = {};
-
 
 setupKeystore((err, keyStuff) => {
   if (err) console.log(err);
   let { pub, prv, keystore } = keyStuff;
 
-pem.createCertificate({ days: 1, selfSigned: true }, function (err, keys) {
-  if (err) {
-    throw err
-  }
+  pem.createCertificate({ days: 1, selfSigned: true }, function (err, keys) {
+    if (err) {
+      throw err
+    }
 
-  const tlsOptions = {
-    key: keys.serviceKey,
-    cert: keys.certificate
-  };
+    const tlsOptions = {
+      key: keys.serviceKey,
+      cert: keys.certificate
+    };
 
-  /**
-   * Fake Auth0 server
-   */
-  const Hapi = require('@hapi/hapi');
-  
-  const init = async () => {
-  
-    const server = Hapi.server({
-      port: 3002,
-      host: '0.0.0.0',
-      tls: tlsOptions,
-      routes: {
-        cors: true
-      }
-    });
+    /**
+     * Fake Auth0 server
+     */
+    const Hapi = require('@hapi/hapi');
 
-//server.state('user', {
-//    ttl: 24 * 60 * 60 * 1000,     // One day
-//    isSecure: true,
-//    isHttpOnly: true,
-//    encoding: 'base64json',
-//    clearInvalid: false,
-//    strictHeader: true
-//});
+    const init = async () => {
 
-    await server.register({
-      plugin: require('hapi-require-https'),
- //     options: {}
-    })
-
-    let _nonce;
-    server.route({
-      method: 'GET',
-      path: '/authorize',
-      handler: (request, h) => {
-        console.log('/authorize');
-        console.log(request.state);
-        console.log(`http://${process.env.SERVER_DOMAIN}/callback?code=AUTHORIZATION_CODE&state=${request.query.state}`);
-//        uri = uri.replace('/authorize?', '');
-//        const parsed = querystring.parse(uri);
-//        state = parsed.state;
-//        nonce = parsed.nonce;
-//        console.log(h);
-
-        _nonce = request.query.nonce;
-
-        return h.redirect(`http://${process.env.SERVER_DOMAIN}/callback?code=AUTHORIZATION_CODE&state=${request.query.state}`);
-      }
-    });
-
-
-//          const oauthTokenScope = nock(`https://${process.env.AUTH0_DOMAIN}`)
-//            .log(console.log)
-//            .post(/oauth\/token/, {
-//                                    'grant_type': 'authorization_code',
-//                                    'redirect_uri': /\/callback/,
-//                                    'client_id': process.env.AUTH0_CLIENT_ID,
-//                                    'client_secret': process.env.AUTH0_CLIENT_SECRET,
-//                                    'code': 'AUTHORIZATION_CODE'
-//                                  })
-//            .reply(200, {
-//              'access_token': signedAccessToken,
-//              'refresh_token': 'SOME_MADE_UP_REFRESH_TOKEN',
-//              'id_token': signedIdToken });
-
-
-    server.route({
-      method: 'POST',
-      path: '/oauth/token',
-      handler: (request, h) => {
-        console.log('/oauth/token...');
-        const signedIdToken = jwt.sign({..._identity,
-                                            aud: process.env.AUTH0_CLIENT_ID,
-                                            iat: Math.floor(Date.now() / 1000) - (60 * 60),
-                                            iss: `https://${process.env.AUTH0_DOMAIN}/`,
-                                            nonce: _nonce },
-                                         prv, { algorithm: 'RS256', header: { kid: keystore.all()[0].kid } })
-
-        return JSON.stringify({
-          'access_token': 'SOME_MADE_UP_ACCESS_TOKEN',
-          'refresh_token': 'SOME_MADE_UP_REFRESH_TOKEN',
-          'id_token': signedIdToken });
-//        return JSON.stringify({
-//              'access_token': signedAccessToken,
-//              'refresh_token': 'SOME_MADE_UP_REFRESH_TOKEN',
-//              'id_token': signedIdToken });
-      }
-    });
-
-
-    server.route({
-      method: 'GET',
-      path: '/.well-known/jwks.json',
-      handler: (request, h) => {
-        console.log('/.well-known/jwks.json');
-        console.log(keystore.toJSON());
-        return JSON.stringify(keystore.toJSON());
-      }
-    });
-
-    server.route({
-      method: 'GET',
-      path: '/userinfo',
-      handler: (request, h) => {
-        console.log('/userinfo');
-        console.log(request.headers);
-
-        // Need to make email dynamic for multi-agent sign-in
-        return { ..._identity};//, email: `agent${identityDb[decoded.sub]}@example.com` };
-      }
-    });
-
-    server.route({
-      method: 'POST',
-      path: '/sign',
-      handler: (request, h) => {
-        console.log('/sign');
-        const signed = jwt.sign(request.payload.accessToken, prv, { algorithm: 'RS256', header: { kid: result.kid } });
-
-        // Add agent to identity "database"
-        if(!identityDb[request.payload.accessToken.sub]) {
-          identityDb[request.payload.accessToken.sub] = Object.keys(identityDb).length + 1;
+      const server = Hapi.server({
+        port: 3002,
+        host: '0.0.0.0',
+        tls: tlsOptions,
+        routes: {
+          cors: true
         }
+      });
 
-        return signed;
-      }
+      await server.register({
+        plugin: require('hapi-require-https'),
+        options: {}
+      })
+
+      /**
+       * Pass an ID token for immediate reference during `/authorize`.  The
+       * authorize route takes a pseudo-random access code and nonce. These
+       * are required to access and validate an ID token. The ID token
+       * registered here is stored to be combined with those codes when the
+       * `/authorize` endpoint is hit after the `/login` redirect. If a new
+       * agent is being added, the login has to happen immediately after in
+       * the tests for the new agent to be added to the "database".
+       */
+      let agentIdToken;
+      server.route({
+        method: 'POST',
+        path: '/register',
+        handler: (request, h) => {
+          console.log('/sign');
+          console.log(request.payload);
+
+          agentIdToken = {...request.payload.token,
+                             aud: process.env.AUTH0_CLIENT_ID,
+                             iss: `https://${process.env.AUTH0_DOMAIN}/`,
+                             iat: Math.floor(Date.now() / 1000) - (60 * 60)},
+
+          console.log(agentIdToken);
+
+          return h.response(agentIdToken);
+        }
+      });
+
+      /**
+       * Hit immediately after `/login`. If a new test agent has been
+       * registered, he is added to the database here.
+       */
+      let _nonce;
+      server.route({
+        method: 'GET',
+        path: '/authorize',
+        handler: (request, h) => {
+          console.log('/authorize');
+          console.log(request.state);
+          console.log(request.query);
+
+          _nonce = request.query.nonce;
+
+          const buffer = crypto.randomBytes(12);
+          const authorizationCode = buffer.toString('hex');
+
+          // A test agent has been registered.
+          if (agentIdToken) {
+            const idToken = { ...agentIdToken, nonce: _nonce };
+            const signedToken = jwt.sign(idToken, prv, { algorithm: 'RS256', header: { kid: keystore.all()[0].kid } });
+
+            identityDb[authorizationCode] = { idToken: idToken,
+                                              count: Object.keys(identityDb).length + 1,
+                                              signedToken: signedToken };
+
+            // This allows the next test to register an agent
+            agentIdToken = undefined;
+          }
+
+          const redirectUrl= `http://${process.env.SERVER_DOMAIN}/callback?code=${authorizationCode}&state=${request.query.state}`;
+          console.log(`Redirecting: ${redirectUrl}`);
+          return h.redirect(redirectUrl);
+        }
+      });
+
+      /**
+       * It's here that you kind of start to understand and appreciate OAuth...
+       */
+      server.route({
+        method: 'POST',
+        path: '/oauth/token',
+        handler: (request, h) => {
+          console.log('/oauth/token...');
+          console.log(request.headers);
+          console.log(request.payload);
+
+          let signedIdToken;
+          if (identityDb[request.payload.code]) {
+            signedIdToken = identityDb[request.payload.code].signedToken;
+          }
+          else {
+            // This step satisfies the earliest client-side auth tests,
+            // which I am too sentimental to delete.
+            signedIdToken = jwt.sign({..._identity,
+                                         aud: process.env.AUTH0_CLIENT_ID,
+                                         iat: Math.floor(Date.now() / 1000) - (60 * 60),
+                                         iss: `https://${process.env.AUTH0_DOMAIN}/`,
+                                         nonce: identityDb[request.payload.code] ? identityDb[request.payload.code].idToken.nonce : _nonce} ,
+                                      prv, { algorithm: 'RS256', header: { kid: keystore.all()[0].kid } })
+          }
+
+          // This was expedient. No problems so far
+          const pretendValidAccessToken = request.payload.code;
+
+          return h.response({
+            'access_token': pretendValidAccessToken,
+            'refresh_token': 'SOME_MADE_UP_REFRESH_TOKEN',
+            'id_token': signedIdToken
+          });
+        }
+      });
+
+      /**
+       * This will come in handing if we switch our
+       * auth strategy again
+       */
+      server.route({
+        method: 'GET',
+        path: '/.well-known/jwks.json',
+        handler: (request, h) => {
+          console.log('/.well-known/jwks.json');
+          console.log(keystore.toJSON());
+          return JSON.stringify(keystore.toJSON());
+        }
+      });
+
+      /**
+       * Delivers as promised
+       */
+      server.route({
+        method: 'GET',
+        path: '/userinfo',
+        handler: (request, h) => {
+          console.log('/userinfo');
+          console.log(request.headers);
+          console.log(request.query);
+
+          let idToken = identityDb[request.query.access_token] ? identityDb[request.query.access_token].idToken : _identity;
+          return h.response(idToken);
+        }
+      });
+
+      await server.start();
+      console.log('Server running on %s', server.info.uri);
+    };
+
+    process.on('unhandledRejection', (err) => {
+      console.log(err);
+      process.exit(1);
     });
 
-    await server.start();
-    console.log('Server running on %s', server.info.uri);
-  };
-  
-  process.on('unhandledRejection', (err) => {
-    console.log(err);
-    process.exit(1);
+    init();
   });
-  
-  init();
-});
 });
 
