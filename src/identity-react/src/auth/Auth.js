854d54b9e868c93a6582cd3c3f048d3fde073b8d
Daniel Bidulock <daniel_bidulock@sil.org>
2020-02-04 16:55:21 -0700

    Update login/logout functions

--- src/identity-react/src/auth/Auth.js
+++ src/identity-react/src/auth/Auth.js
@@ -4,6 +4,7 @@
  * Shamelessly stolen from https://kentcdodds.com/blog/authentication-in-react-applications
  */
 import React from 'react';
+import { Redirect } from 'react-router-dom';
 
 export const AuthContext = React.createContext();
 
@@ -19,27 +20,33 @@ export function AuthProvider({children}) {
   React.useEffect(() => {
     console.log('login');
     const headers = new Headers();
-//    headers.append('Access-Control-Allow-Credentials', 'true');
-//    headers.append('Access-Control-Allow-Origin', 'true');
+ //   headers.append('Access-Control-Allow-Credentials', 'true');
+//    headers.append('Access-Control-Allow-Origin', ['http://localhost:3001']);//'true');
     //fetch('/login', {method: 'GET', headers: headers, credentials: 'include', mode: 'no-cors' }).then(response => {
     
     //fetch('/login', {method: 'GET', headers: headers, credentials: 'include', mode: 'no-cors', redirect: 'follow' }).then(response => {
-    fetch('/login', {method: 'GET', headers: headers, credentials: 'include' }).then(response => {
+    fetch('/agent', {method: 'GET', headers: headers}).then(response => {
 //       return response.json();
-       return response.text();
+      return response.text();
     }).then(profile => {
       console.log('FETCH RESPONSE');
-      console.log(JSON.stringify(profile));
+      console.log(profile);
+      console.log(JSON.parse(profile).socialProfile.picture);
 //      setState({status: 'success', error: null, agent: profile});
       setState({status: 'success', error: null, agent: profile ? JSON.parse(profile) : {} });
     }).catch(error => {
       console.log('FETCH ERROR', error);
       setState({status: 'error', error, agent: null});
     });
-  }, [])
+  }, []);
+
+  function logout() {
+    setState({status: 'pending', err: null, agent: null});
+    window.location.href = '/logout';
+  };
 
   return (
-    <AuthContext.Provider value={state}>
+    <AuthContext.Provider value={{ ...state, logout: logout}}>
       {state.status === 'pending' ? (
         'Loading...'
       ) : state.status === 'error' ? (
@@ -58,10 +65,10 @@ export function AuthProvider({children}) {
 
 export function useAuthState() {
   const state = React.useContext(AuthContext)
-  const isPending = state.status === 'pending'
-  const isError = state.status === 'error'
-  const isSuccess = state.status === 'success'
-  const isAuthenticated = state.agent && isSuccess
+  const isPending = state.status === 'pending';
+  const isError = state.status === 'error';
+  const isSuccess = state.status === 'success';
+  const isAuthenticated = state.agent && isSuccess;
   return {
     ...state,
     isPending,
