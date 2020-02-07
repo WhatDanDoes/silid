7d72aacf14002f804e0a272aea455b3f2b9c1e26
Daniel Bidulock <daniel_bidulock@sil.org>
2020-02-07 15:12:12 -0700

    Clean up

--- src/silid-server/app.js
+++ src/silid-server/app.js
@@ -15,18 +15,13 @@ const agentRouter = require('./routes/agent');
 const organizationRouter = require('./routes/organization');
 const teamRouter = require('./routes/team');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(cors());
-//app.use(cors({
-//  credentials: true,
-//  headers: ['access-control-allow-origin', 'access-control-allow-credentials']
-//}));
-
 
 app.use(logger('dev'));
 app.use(express.json());
