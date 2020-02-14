db54c6b8b938ad2fe6cbc8b0a016330ca01036fe
Daniel Bidulock <daniel_bidulock@sil.org>
2020-02-14 14:13:46 -0700

    Clean up

--- src/identity-react/src/components/Appbar.tsx
+++ src/identity-react/src/components/Appbar.tsx
@@ -13,7 +13,6 @@ import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import InboxIcon from '@material-ui/icons/MoveToInbox';
-import MailIcon from '@material-ui/icons/Mail';
 import Avatar from '@material-ui/core/Avatar';
 import Grid from '@material-ui/core/Grid';
 import { useAuthState } from '../auth/Auth';
