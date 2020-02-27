0b5f1c01a4e014ec066b19432008cde87fd048e7
Daniel Bidulock <daniel_bidulock@sil.org>
2020-02-27 12:38:20 -0700

    Remove superfluous New Team button

--- src/identity-react/src/routes/Team.tsx
+++ src/identity-react/src/routes/Team.tsx
@@ -3,15 +3,11 @@ import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
-import Fab from '@material-ui/core/Fab';
-import AddIcon from '@material-ui/icons/Add';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-import Button from '@material-ui/core/Button';
 import Flash from '../components/Flash';
-import TeamCreateForm from '../components/TeamCreateForm';
 // Remove this junk later
 import InboxIcon from '@material-ui/icons/MoveToInbox';
 
@@ -36,9 +32,8 @@ const useStyles = makeStyles((theme: Theme) =>
 );
 
 const Team = (props: any) => {
-  const [formVisible, toggleFormVisible] = useState(false);
   const [teamList, setTeamList] = useState<Teams>({ results: [] } as Teams);
-  const [flashProps, setFlashProps] = useState({} as any);
+  const [flashProps] = useState({} as any);
 
   const classes = useStyles();
   const service = useGetTeamService();
@@ -62,31 +57,6 @@ const Team = (props: any) => {
           </Typography>
           { props.location.state ? <Flash message={props.location.state} variant="success" /> : '' }
           { flashProps.errors ? flashProps.errors.map(error => <Flash message={error.message} variant={flashProps.variant} />) : '' }
-          { formVisible ?
-              <React.Fragment>
-                <TeamCreateForm
-                  done={(results) => {
-                    toggleFormVisible(false);
-                    if (results.errors) {
-                      setFlashProps({errors: results.errors, variant: 'error' });
-                    }
-                    else {
-                      setTeamList({ results: [results, ...teamList.results] } as Teams);
-                    }
-                  }}/>
-                <Button id="cancel-changes"
-                  variant="contained" color="secondary"
-                  onClick={() => {
-                    toggleFormVisible(false)
-                  }}>
-                    Cancel
-                </Button>
-              </React.Fragment>
-            :
-              <Fab id="add-team" color="secondary" aria-label="add" className={classes.margin}>
-                <AddIcon onClick={() => toggleFormVisible(true)} />
-              </Fab>
-          }
 
           <Typography variant="body2" color="textSecondary" component="p">
           {service.status === 'loading' && <div>Loading...</div>}
