a246adc00cbf2658952721fa032146889eb7b706
Matthew Reynolds <mattrzr@gmail.com>
2020-03-10 17:45:02 -0500

    Add aws-sdk transport for development_aws and production environment

--- src/silid-server/mailer/index.js
+++ src/silid-server/mailer/index.js
@@ -1,12 +1,13 @@
 'use strict';
 
-const nodemailer = require('nodemailer');
+let nodemailer = require('nodemailer');
+let aws = require('aws-sdk');
 
-module.exports = function() {
+module.exports = (function() {
   let env = process.env.NODE_ENV || 'development';
   let transport;
 
-  if (env === 'production' || env === 'staging') {
+  if (env === 'staging') {
     transport = {
       service: 'gmail',
       auth: {
@@ -14,20 +15,26 @@ module.exports = function() {
         pass: process.env.NOREPLY_PASSWORD
       }
     };
-  }
-  else if (env === 'development') {
+  } else if (env === 'development') {
     transport = {
       port: 25,
       ignoreTLS: true
     };
-  }
-  else {
+  } else if (env === 'development_aws' || env === 'production') {
+    // configure AWS SDK
+    aws.config.loadFromPath('config/aws.json');
+    transport = {
+      SES: new aws.SES({
+        apiVersion: '2010-12-01'
+      })
+    };
+  } else {
     transport = require('nodemailer-mock-transport')({
       foo: 'bar'
     });
   }
 
   const transporter = nodemailer.createTransport(transport);
- 
+
   return { transporter: transporter, transport: transport };
-}();
+})();
