1e3790aaa7c693cc525d4000a455b478b0ecfddc
Daniel Bidulock <daniel_bidulock@sil.org>
2020-03-12 14:11:19 -0600

    Make endpoint url dynamic according to admin role

--- src/identity-react/src/services/useGetTeamService.ts
+++ src/identity-react/src/services/useGetTeamService.ts
@@ -1,12 +1,15 @@
 import { useEffect, useState } from 'react';
 import { Service } from '../types/Service';
 import { Team } from '../types/Team';
+import { useAdminState } from '../auth/Admin';
 
 export interface Teams {
   results: Team[];
 }
 
 const useTeamService = () => {
+  const admin = useAdminState();
+
   const [result, setResult] = useState<Service<Teams>>({
     status: 'loading'
   });
@@ -15,11 +18,16 @@ const useTeamService = () => {
     const headers = new Headers();
     headers.append('Content-Type', 'application/json; charset=utf-8');
 
-    fetch(`/team`, { headers, credentials: 'include', mode: 'no-cors' })
+    let url = '/team';
+    if (admin.isEnabled) {
+      url += '/admin';
+    }
+
+    fetch(url, { headers, credentials: 'include', mode: 'no-cors' })
       .then(response => response.json())
       .then(response => setResult({ status: 'loaded', payload: { results: response } }))
       .catch(error => setResult({ status: 'error', error }));
-  }, []);
+  }, [admin.isEnabled]);
 
   return result;
 };
