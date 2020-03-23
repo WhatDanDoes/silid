508ff3f82650de6df3c758b8c876b65644c589a1
Daniel Bidulock <daniel_bidulock@sil.org>
2020-03-23 17:12:46 -0600

    Enable all-access for configured root agent

--- src/silid-server/lib/checkPermissions.js
+++ src/silid-server/lib/checkPermissions.js
@@ -13,69 +13,104 @@ const checkPermissions = function(permissions) {
       return res.redirect('/login')
     }
 
-    jwtAuthz(permissions, { failWithError: true, checkAllScopes: true })(req, res, err => {
-      if (err) {
-        return res.status(err.statusCode).json({...err, message: 'Forbidden' });
-      }
+    const socialProfile = req.user._json;
 
-      const socialProfile = req.user._json;
+    models.Agent.findOne({ where: { email: socialProfile.email } }).then(agent => {
+      req.agent = agent;
 
-      models.Agent.findOne({ where: { email: socialProfile.email } }).then(agent => {
-        req.agent = agent;
-
-        // Fill in any blank agent columns with social profile data
-        const updates = {};
-        if (req.agent) {
-          for (let key in socialProfile) {
-            if (agent[key] === null) {
-              updates[key] = socialProfile[key];
-            }
+      // Fill in any blank agent columns with social profile data
+      const updates = {};
+      if (req.agent) {
+        for (let key in socialProfile) {
+          if (agent[key] === null) {
+            updates[key] = socialProfile[key];
           }
         }
+      }
 
-        if (!req.agent || JSON.stringify(req.agent.socialProfile) !== JSON.stringify(socialProfile)) {
+      if (!req.agent || JSON.stringify(req.agent.socialProfile) !== JSON.stringify(socialProfile)) {
+        models.Agent.update(
+          { socialProfile: socialProfile, ...updates },
+          { returning: true, where: { email: socialProfile.email } }).then(function([rowsUpdate, [updatedAgent]]) {
+
+          if (updatedAgent) {
+            req.agent = updatedAgent;
+
+            if (req.agent.isSuper) {
+              return next();
+            }
+
+            jwtAuthz(permissions, { failWithError: true, checkAllScopes: true })(req, res, err => {
+              if (err) {
+                return res.status(err.statusCode).json(err);
+              }
+
+              next();
+            });
+          }
+          else {
+            models.Agent.create({ name: socialProfile.name, email: socialProfile.email, socialProfile: socialProfile }).then(agent => {
+              req.agent = agent;
+
+              if (req.agent.isSuper) {
+                return next();
+              }
+
+              jwtAuthz(permissions, { failWithError: true, checkAllScopes: true })(req, res, err => {
+                if (err) {
+                  return res.status(err.statusCode).json(err);
+                }
+
+                next();
+              });
+            }).catch(err => {
+              res.status(500).json(err);
+            });
+          }
+        }).catch(err => {
+          res.status(500).json(err);
+        });
+      }
+      else {
+        if (Object.keys(updates).length) {
           models.Agent.update(
-            { socialProfile: socialProfile, ...updates },
+            { updates },
             { returning: true, where: { email: socialProfile.email } }).then(function([rowsUpdate, [updatedAgent]]) {
 
             if (updatedAgent) {
               req.agent = updatedAgent;
+            }
+
+            if (req.agent.isSuper) {
+              return next();
+            }
+
+            jwtAuthz(permissions, { failWithError: true, checkAllScopes: true })(req, res, err => {
+              if (err) {
+                return res.status(err.statusCode).json(err);
+              }
               next();
-            }
-            else {
-              models.Agent.create({ name: socialProfile.name, email: socialProfile.email, socialProfile: socialProfile }).then(agent => {
-                req.agent = agent;
-                next();
-              }).catch(err => {
-                res.status(500).json(err);
-              });
-            }
+            });
           }).catch(err => {
             res.status(500).json(err);
           });
         }
         else {
-          if (Object.keys(updates).length) {
-            models.Agent.update(
-              { updates },
-              { returning: true, where: { email: socialProfile.email } }).then(function([rowsUpdate, [updatedAgent]]) {
-
-              if (updatedAgent) {
-                req.agent = updatedAgent;
-              }
-
-              next();
-            }).catch(err => {
-              res.status(500).json(err);
-            });
+          if (req.agent.isSuper) {
+            return next();
           }
-          else {
+
+          jwtAuthz(permissions, { failWithError: true, checkAllScopes: true })(req, res, err => {
+            if (err) {
+              return res.status(err.statusCode).json(err);
+            }
+
             next();
-          }
+          });
         }
-      }).catch(err => {
-        res.status(500).json(err);
-      });
+      }
+    }).catch(err => {
+      res.status(500).json(err);
     });
   };
 }
