1afcadfd9cec65b735ccb57927e464564a27d67e
Daniel Bidulock <daniel_bidulock@sil.org>
2020-03-31 17:16:08 -0600

    Get agent listing cache access tests

--- src/identity-react/cypress/integration/root/agentIndexSpec.js
+++ src/identity-react/cypress/integration/root/agentIndexSpec.js
@@ -10,11 +10,11 @@ context('root/Agent Index', function() {
     // Root email set in `silid-server/.env`
     _profile = {...this.profile, email: 'root@example.com'};
   });
- 
+
   afterEach(() => {
     cy.task('query', 'TRUNCATE TABLE "Agents" CASCADE;');
   });
-  
+
   describe('unauthenticated', done => {
     beforeEach(() => {
       cy.visit('/#/agent/admin');
@@ -45,57 +45,78 @@ context('root/Agent Index', function() {
 
     context('admin mode', () => {
 
-      beforeEach(function() {
-        cy.get('#app-menu-button').click();
-        cy.get('#admin-switch').check();
-        cy.contains('Agent Directory').click();
-      });
- 
-      it('lands in the right spot', () => {
-        cy.url().should('match', /\/#\/agent\/admin$/);
-      });
+      context('cached mode', () => {
 
-      it('displays common Agent Directory interface elements', function() {
-        cy.get('h3').contains('Directory');
-      });
+        context('switched on', () => {
+          beforeEach(function() {
+            cy.get('#app-menu-button').click();
+            cy.get('#admin-switch').check();
+            cy.get('#show-cached-switch').check();
+            cy.contains('Agent Directory').click();
+          });
 
-      describe('agent directory list', () => {
-        context('no agents', () => {
-          it('displays only the root agent', () => {
-            cy.task('query', 'SELECT * FROM "Agents";').then(([results, metadata]) => {
-              expect(results.length).to.equal(1);
-              cy.get('#agent-list').should('exist');
-              cy.get('#agent-list').find('.agent-button').its('length').should('eq', 1);
-              cy.get('.agent-button').first().contains(results[0].name);
-              cy.get('.agent-button a').first().should('have.attr', 'href').and('include', `#agent/${results[0].id}`)
+          it('lands in the right spot', () => {
+            cy.url().should('match', /\/#\/agent\/admin$/);
+          });
+
+          it('displays common Agent Directory interface elements', function() {
+            cy.get('h3').contains('Directory');
+          });
+
+          describe('agent directory list', () => {
+            context('no agents', () => {
+              it('displays only the root agent', () => {
+                cy.task('query', 'SELECT * FROM "Agents";').then(([results, metadata]) => {
+                  expect(results.length).to.equal(1);
+                  cy.get('#agent-list').should('exist');
+                  cy.get('#agent-list').find('.agent-button').its('length').should('eq', 1);
+                  cy.get('.agent-button').first().contains(results[0].name);
+                  cy.get('.agent-button a').first().should('have.attr', 'href').and('include', `#agent/${results[0].id}`)
+                });
+              });
+            });
+
+            context('some agents', () => {
+
+              let agents;
+              beforeEach(function() {
+                // Easy way to register a new agent
+                cy.login('someotherguy@example.com', { ..._profile, name: 'Some Other Guy' }, [this.scope.read.agents]);
+                cy.login(_profile.email, _profile);
+                cy.task('query', 'SELECT * FROM "Agents";').then(([results, metadata]) => {
+                  agents = results;
+                  expect(agents.length).to.equal(2);
+
+                  cy.get('#app-menu-button').click();
+                  cy.get('#admin-switch').check();
+                  cy.get('#show-cached-switch').check();
+                  cy.contains('Directory').click();
+                });
+              });
+
+              it('displays the agents', () => {
+                cy.get('#agent-list').should('exist');
+                cy.get('#agent-list').find('.agent-button').its('length').should('eq', 2);
+                cy.get('.agent-button').first().contains(agents[0].name);
+                cy.get('.agent-button a').first().should('have.attr', 'href').and('include', `#agent/${agents[0].id}`)
+                cy.get('.agent-button').last().contains(agents[1].name);
+                cy.get('.agent-button a').last().should('have.attr', 'href').and('include', `#agent/${agents[1].id}`)
+              });
             });
           });
         });
 
-        context('some agents', () => {
-
-          let agents;
+        context('switched off', () => {
           beforeEach(function() {
-            // Easy way to register a new agent
-            cy.login('someotherguy@example.com', { ..._profile, name: 'Some Other Guy' }, [this.scope.read.agents]);
-            cy.login(_profile.email, _profile);
-            cy.task('query', 'SELECT * FROM "Agents";').then(([results, metadata]) => {
-              agents = results;
-              expect(agents.length).to.equal(2);
-
-              cy.get('#app-menu-button').click();
-              cy.get('#admin-switch').check();
-              cy.contains('Directory').click();
-            });
+            cy.get('#app-menu-button').click();
+            cy.get('#admin-switch').check();
+            cy.get('#show-cached-switch').uncheck();
+            cy.contains('Agent Directory').click();
           });
 
           it('displays the agents', () => {
             cy.get('#agent-list').should('exist');
-            cy.get('#agent-list').find('.agent-button').its('length').should('eq', 2);
-            cy.get('.agent-button').first().contains(agents[0].name);
-            cy.get('.agent-button a').first().should('have.attr', 'href').and('include', `#agent/${agents[0].id}`)
-            cy.get('.agent-button').last().contains(agents[1].name);
-            cy.get('.agent-button a').last().should('have.attr', 'href').and('include', `#agent/${agents[1].id}`)
+            cy.get('#agent-list').find('.agent-button').its('length').should('eq', 1);
           });
         });
       });
