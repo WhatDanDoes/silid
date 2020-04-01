460210635f917b656c13a9bd27594d4f4182db98
Daniel Bidulock <daniel_bidulock@sil.org>
2020-04-01 14:35:32 -0600

    Get Flash message for insufficient scope

--- src/identity-react/cypress/integration/organizationIndexSpec.js
+++ src/identity-react/cypress/integration/organizationIndexSpec.js
@@ -33,110 +33,128 @@ context('Organization', function() {
   });
 
   describe('authenticated', () => {
+    context('without sufficient scope', () => {
+      let agent;
+      beforeEach(function() {
+        cy.login(_profile.email, _profile, [this.scope.read.agents]);
+        cy.wait(300);
+      });
 
-    let agent;
-    beforeEach(function() {
-      cy.login(_profile.email, _profile, [this.scope.read.agents,
-                                          this.scope.create.organizations,
-                                          this.scope.read.organizations]);
+      it('lands in the right spot', () => {
+        cy.url().should('contain', '/#/organization');
+      });
 
-      cy.get('#app-menu-button').click();
-      cy.get('#organization-button').click().then(() =>  {
-        cy.task('query', `SELECT * FROM "Agents" WHERE "email"='${_profile.email}' LIMIT 1;`).then(([results, metadata]) => {
-          agent = results[0];
-        });
+      it('displays a friendly message', () => {
+        cy.get('h3').contains('Organizations');
+        cy.contains('Insufficient scope');
       });
     });
 
-    afterEach(() => {
-      cy.task('query', 'TRUNCATE TABLE "Organizations" CASCADE;');
-    });
+    context('with sufficient scope', () => {
+      let agent;
+      beforeEach(function() {
+        cy.login(_profile.email, _profile, [this.scope.read.agents,
+                                            this.scope.create.organizations,
+                                            this.scope.read.organizations]);
 
-    it('lands in the right spot', () => {
-      cy.url().should('contain', '/#/organization');
-    });
-
-    it('displays common Organization interface elements', function() {
-      cy.get('h3').contains('Organizations');
-      cy.get('button#add-organization').should('exist');
-    });
-
-    describe('organization membership', () => {
-      context('no organizations', () => {
-        it('displays no organizations', () => {
-          cy.task('query', 'SELECT * FROM "Organizations";').then(([results, metadata]) => {
-            expect(results.length).to.equal(0);
-            cy.get('#organization-list').should('not.exist');
+        cy.get('#app-menu-button').click();
+        cy.get('#organization-button').click().then(() =>  {
+          cy.task('query', `SELECT * FROM "Agents" WHERE "email"='${_profile.email}' LIMIT 1;`).then(([results, metadata]) => {
+            agent = results[0];
           });
         });
       });
 
-      context('some organizations', () => {
-        let organization;
-        beforeEach(function() {
+      afterEach(() => {
+        cy.task('query', 'TRUNCATE TABLE "Organizations" CASCADE;');
+      });
 
-          // Create an organization with another agent
-          cy.login('someotherguy@example.com', _profile, [this.scope.read.agents,
-                                                          this.scope.create.organizations,
-                                                          this.scope.update.organizations,
-                                                          this.scope.create.organizationMembers]);
-          cy.request({ url: '/organization',  method: 'POST', body: { name: 'One Book Canada' } }).then(org => {
-            organization = org.body;
+      it('lands in the right spot', () => {
+        cy.url().should('contain', '/#/organization');
+      });
 
-            // Add member agent
-            cy.request({ url: '/organization', method: 'PATCH', body: { id: organization.id, memberId: agent.id } }).then((res) => {
+      it('displays common Organization interface elements', function() {
+        cy.get('h3').contains('Organizations');
+        cy.get('button#add-organization').should('exist');
+      });
 
-              // Login member agent
-              cy.login(_profile.email, _profile, [this.scope.read.agents,
-                                                  this.scope.read.organizations]);
+      describe('organization membership', () => {
+        context('no organizations', () => {
+          it('displays no organizations', () => {
+            cy.task('query', 'SELECT * FROM "Organizations";').then(([results, metadata]) => {
+              expect(results.length).to.equal(0);
+              cy.get('#organization-list').should('not.exist');
+            });
+          });
+        });
 
+        context('some organizations', () => {
+          let organization;
+          beforeEach(function() {
+
+            // Create an organization with another agent
+            cy.login('someotherguy@example.com', _profile, [this.scope.read.agents,
+                                                            this.scope.create.organizations,
+                                                            this.scope.update.organizations,
+                                                            this.scope.create.organizationMembers]);
+            cy.request({ url: '/organization',  method: 'POST', body: { name: 'One Book Canada' } }).then(org => {
+              organization = org.body;
+
+              // Add member agent
+              cy.request({ url: '/organization', method: 'PATCH', body: { id: organization.id, memberId: agent.id } }).then((res) => {
+
+                // Login member agent
+                cy.login(_profile.email, _profile, [this.scope.read.agents,
+                                                    this.scope.read.organizations]);
+
+                cy.visit('/#/');
+                cy.get('#app-menu-button').click();
+                cy.get('#organization-button').click();
+              });
+            });
+          });
+
+          it('displays a list of organizations', () => {
+            cy.get('#organization-list').should('exist');
+            cy.get('#organization-list').find('.organization-button').its('length').should('eq', 1);
+            cy.get('.organization-button').first().contains('One Book Canada');
+            cy.get('.organization-button a').first().should('have.attr', 'href').and('include', `#organization/${organization.id}`)
+          });
+        });
+      });
+
+      describe('organization creator', () => {
+
+        context('no organizations created by this agent', () => {
+          it('displays no organizations', () => {
+            cy.task('query', `SELECT * FROM "Organizations" WHERE "creatorId"=${agent.id};`).then(([results, metadata]) => {;
+              expect(results.length).to.equal(0);
+              cy.get('#organization-list').should('not.exist');
+            });
+          });
+        });
+
+        context('agent has created organizations', () => {
+
+          let organization;
+          beforeEach(function() {
+            cy.request({ url: '/organization',  method: 'POST', body: { name: 'One Book Canada' } }).then(org => {
+              organization = org.body;
               cy.visit('/#/');
               cy.get('#app-menu-button').click();
               cy.get('#organization-button').click();
             });
           });
-        });
 
-        it('displays a list of organizations', () => {
-          cy.get('#organization-list').should('exist');
-          cy.get('#organization-list').find('.organization-button').its('length').should('eq', 1);
-          cy.get('.organization-button').first().contains('One Book Canada');
-          cy.get('.organization-button a').first().should('have.attr', 'href').and('include', `#organization/${organization.id}`)
-        });
-      });
-    });
+          it('displays a list of organizations', () => {
+            cy.request({ url: '/organization',  method: 'GET' }).then(orgs => {
+              expect(orgs.body.length).to.eq(1);
 
-    describe('organization creator', () => {
-
-      context('no organizations created by this agent', () => {
-        it('displays no organizations', () => {
-          cy.task('query', `SELECT * FROM "Organizations" WHERE "creatorId"=${agent.id};`).then(([results, metadata]) => {;
-            expect(results.length).to.equal(0);
-            cy.get('#organization-list').should('not.exist');
-          });
-        });
-      });
-
-      context('agent has created organizations', () => {
-
-        let organization;
-        beforeEach(function() {
-          cy.request({ url: '/organization',  method: 'POST', body: { name: 'One Book Canada' } }).then(org => {
-            organization = org.body;
-            cy.visit('/#/');
-            cy.get('#app-menu-button').click();
-            cy.get('#organization-button').click();
-          });
-        });
-
-        it('displays a list of organizations', () => {
-          cy.request({ url: '/organization',  method: 'GET' }).then(orgs => {
-            expect(orgs.body.length).to.eq(1);
-
-            cy.get('#organization-list').should('exist');
-            cy.get('#organization-list').find('.organization-button').its('length').should('eq', 1);
-            cy.get('.organization-button').first().contains('One Book Canada');
-            cy.get('.organization-button a').first().should('have.attr', 'href').and('include', `#organization/${organization.id}`)
+              cy.get('#organization-list').should('exist');
+              cy.get('#organization-list').find('.organization-button').its('length').should('eq', 1);
+              cy.get('.organization-button').first().contains('One Book Canada');
+              cy.get('.organization-button a').first().should('have.attr', 'href').and('include', `#organization/${organization.id}`)
+            });
           });
         });
       });
