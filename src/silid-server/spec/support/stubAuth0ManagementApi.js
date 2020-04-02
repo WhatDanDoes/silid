d3b7c8a2b151b92304378380b4216b77aa8b395a
Daniel Bidulock <daniel_bidulock@sil.org>
2020-04-02 15:48:35 -0600

    Repurpose for general frequent-use stubbing

--- src/silid-server/spec/support/stubAuth0ManagementApi.js
+++ src/silid-server/spec/support/stubAuth0ManagementApi.js
@@ -1,156 +1,28 @@
-const nock = require('nock');
-
 /**
- * 2019-11-13
- * Sample tokens taken from:
+ * Stub all the commonly used Auth0 Management API endpoints
  *
- * https://auth0.com/docs/api-auth/tutorials/adoption/api-tokens
- *
- * For the moment, it doesn't seem to matter that all authenticated
- * agents are using the same access token for testing purposes.
- */
-const _access = require('../fixtures/sample-auth0-access-token');
-_access.iss = `http://${process.env.AUTH0_DOMAIN}/`;
-
-const jwt = require('jsonwebtoken');
-
-/**
- * Stub all the Auth0 Management API stuff. Used on a test-by-test basis to
- * provide and test against scoped permissions. Ensure the API is being called
- * when appropriate and likewise _not_ being called when appropriate
- *
- * @param array
  * @param function
  */
-module.exports = function(permissions, done) {
+const apiScope = require('../../config/apiPermissions');
+const stubAuth0ManagementEndpoint = require('../support/stubAuth0ManagementEndpoint');
 
-  require('./setupKeystore').then(singleton => {
-    let { pub, prv, keystore } = singleton.keyStuff;
+module.exports = function(done) {
 
-    /**
-     * A new agent needs some basic permissions. This endpoint is called
-     * when `silid-server` needs permission to set these permissions
-     */
-    let accessToken = jwt.sign({..._access, scope: permissions},
-                               prv, { algorithm: 'RS256', header: { kid: keystore.all()[0].kid } })
-    const oauthTokenScope = nock(`https://${process.env.AUTH0_DOMAIN}`)
-      .log(console.log)
-      .post(/oauth\/token/, {
-                              'grant_type': 'client_credentials',
-                              'client_id': process.env.AUTH0_CLIENT_ID,
-                              'client_secret': process.env.AUTH0_CLIENT_SECRET,
-                              'audience': `https://${process.env.AUTH0_DOMAIN}/api/v2/`,
-                              'scope': permissions.join(' ')
-                            })
-      .reply(200, {
-        'access_token': accessToken,
-        'token_type': 'Bearer',
-      });
+  let auth0GetRolesScope, auth0UserAssignRolesScope, oauthTokenScope;
 
-    /**
-     * GET `/users`
-     */
-    const auth0UserListScope = nock(`https://${process.env.AUTH0_DOMAIN}`, { reqheaders: { authorization: `Bearer ${accessToken}`} })
-      .log(console.log)
-      .get('/api/v2/users')
-      .reply(200, (uri, requestBody, cb) => {
-        cb(null, require('../fixtures/managementApi/userList'));
-      });
+  /**
+   * The top two layers account stub the endpoints required for the `viewer`
+   * role check in `lib/checkPermissions`
+   */
+  stubAuth0ManagementEndpoint([apiScope.read.roles], (err, apiScopes) => {
+    if (err) return done(err);
+    ({auth0GetRolesScope} = apiScopes);
 
-    /**
-     * POST `/users`
-     *
-     * Auth0 requires a connection for this endpoint. It is called `Initial-Connection`
-     * here. This setting can be configured at:
-     *
-     * https://manage.auth0.com/dashboard/us/silid/connections
-     */
-    const auth0UserCreateScope = nock(`https://${process.env.AUTH0_DOMAIN}`, { reqheaders: { authorization: `Bearer ${accessToken}`} })
-      .log(console.log)
-      .post(/api\/v2\/users/, {
-                              'email': /.+/i,
-                              'connection': 'Initial-Connection',
-                            })
-      .reply(201, {
-        "user_id": "auth0|507f1f77bcf86c0000000000",
-        "email": "doesnotreallymatterforthemoment@example.com",
-        "email_verified": false,
-        "identities": [
-          {
-            "connection": "Initial-Connection",
-          }
-        ]
-      });
+    stubAuth0ManagementEndpoint([apiScope.read.roles, apiScope.update.users], (err, apiScopes) => {
+      if (err) return done(err);
+      ({auth0UserAssignRolesScope, oauthTokenScope} = apiScopes);
 
-    /**
-     * PATCH `/users`
-     */
-    const auth0UserUpdateScope = nock(`https://${process.env.AUTH0_DOMAIN}`, { reqheaders: { authorization: `Bearer ${accessToken}`} })
-      .log(console.log)
-      .patch(/api\/v2\/users\/*/)
-      .reply(201, {
-        "user_id": "auth0|507f1f77bcf86c0000000000",
-        "email": "doesnotreallymatterforthemoment@example.com",
-        "email_verified": false,
-        "identities": [
-          {
-            "connection": "Initial-Connection",
-          }
-        ]
-      });
-
-    /**
-     * DELETE `/users`
-     *
-     */
-    const auth0UserDeleteScope = nock(`https://${process.env.AUTH0_DOMAIN}`, { reqheaders: { authorization: `Bearer ${accessToken}`} })
-      .log(console.log)
-      .delete(/api\/v2\/users\/*/)
-      .reply(201, {
-        "user_id": "auth0|507f1f77bcf86c0000000000",
-        "email": "doesnotreallymatterforthemoment@example.com",
-        "email_verified": false,
-        "identities": [
-          {
-            "connection": "Initial-Connection",
-          }
-        ]
-      });
-
-    /**
-     * GET `/users-by-email`
-     */
-    const auth0UserReadScope = nock(`https://${process.env.AUTH0_DOMAIN}`, { reqheaders: { authorization: `Bearer ${accessToken}`} })
-      .log(console.log)
-      .get(/api\/v2\/users-by-email/)
-      .query({ 'email': /.+/i })
-      .reply(200, {
-        "user_id": "auth0|507f1f77bcf86c0000000000",
-        "email": "doesnotreallymatterforthemoment@example.com",
-        "email_verified": false,
-        "identities": [
-          {
-            "connection": "Initial-Connection",
-          }
-        ]
-      });
-
-    /**
-     * GET `/users/:id/roles`
-     */
-    const auth0UserAssignRolesScope = nock(`https://${process.env.AUTH0_DOMAIN}`, { reqheaders: { authorization: `Bearer ${accessToken}`} })
-      .log(console.log)
-      .post(/api\/v2\/users\/.+\/roles/, {
-                              'roles': /.+/i,
-      })
-      .reply(200, { });
-
-
-
-    done(null, {
-                oauthTokenScope,
-                auth0UserCreateScope, auth0UserReadScope, auth0UserUpdateScope, auth0UserDeleteScope, auth0UserListScope,
-                auth0UserAssignRolesScope,
+      done(null, {auth0GetRolesScope, auth0UserAssignRolesScope, oauthTokenScope});
     });
   });
 };
