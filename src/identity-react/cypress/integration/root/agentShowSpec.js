8ace4030bf3ec11c96db1c36b0183289e0691d7a
Daniel Bidulock <daniel_bidulock@sil.org>
2020-04-07 14:07:51 -0600

    Test social data JSON display and update routes

--- src/identity-react/cypress/integration/root/agentShowSpec.js
+++ src/identity-react/cypress/integration/root/agentShowSpec.js
@@ -15,7 +15,7 @@ context('root/Agent show', function() {
   afterEach(() => {
     cy.task('query', 'TRUNCATE TABLE "Agents" CASCADE;');
   });
-  
+
   describe('unauthenticated', done => {
     beforeEach(() => {
       cy.visit(`/#/agent/333`);
@@ -56,15 +56,17 @@ context('root/Agent show', function() {
             cy.login(_profile.email, _profile);
             cy.get('#app-menu-button').click();
             cy.get('#admin-switch').check();
-            cy.visit(`/#/agent/${memberAgent.id}`);
-            cy.wait(500);
+            cy.contains('Agent Directory').click();
+            cy.wait(200);
+            cy.get('.agent-button a').first().click();
+            cy.wait(200);
           });
 
           it('lands in the right spot', () => {
-            cy.url().should('contain', `/#/agent/${memberAgent.id}`);
+            cy.url().should('contain', `/#/agent/${memberAgent.socialProfile.id}`);
           });
 
-          it('displays agent social profile info in form', function() {
+          it('displays agent\'s editable info in form', function() {
             cy.get('h3').contains('Profile');
             cy.get('input[name="name"][type="text"]').should('have.value', memberAgent.name);
             cy.get('input[name="name"][type="text"]').should('not.be.disabled');
@@ -76,6 +78,16 @@ context('root/Agent show', function() {
           it('disables the Save button', () => {
             cy.get('button[type="submit"]').should('be.disabled');
           });
+
+          describe('social profile data', () => {
+            it('displays JSON', () => {
+              cy.get('.react-json-view').its('length').should('eq', 1);
+              cy.get('.react-json-view').contains('locale');
+              cy.get('.react-json-view').contains('picture');
+              cy.get('.react-json-view').contains('user_id');
+              cy.get('.react-json-view').contains('name');
+            });
+          });
         });
 
         describe('viewing your own profile', () => {
@@ -85,25 +97,35 @@ context('root/Agent show', function() {
             cy.login(_profile.email, _profile);
             cy.get('#app-menu-button').click();
             cy.get('#admin-switch').check();
-            cy.get('#agent-button').contains('Profile').click();
+            cy.wait(200);
             cy.task('query', `SELECT * FROM "Agents" WHERE "email"='${_profile.email}' LIMIT 1;`).then(([results, metadata]) => {
               root = results[0];
-              cy.visit(`/#/agent/${root.id}`);
-              cy.wait(500);
+              cy.visit(`/#/agent/${root.socialProfile.id}`);
+              cy.wait(200);
             });
           });
 
           it('lands in the right spot', () => {
-            cy.url().should('contain', `/#/agent/${root.id}`);
+            cy.url().should('contain', `/#/agent/${root.socialProfile.id}`);
           });
 
-          it('displays agent social profile info in form', () => {
+          it('displays agent\'s editable info in form', () => {
             cy.get('h3').contains('Profile');
             cy.get('input[name="name"][type="text"]').should('have.value', root.name);
             cy.get('input[name="name"][type="text"]').should('not.be.disabled');
             cy.get('input[name="email"][type="email"]').should('have.value', root.email);
             cy.get('input[name="email"][type="email"]').should('be.disabled');
             cy.get('button[type="submit"]').should('exist');
+          });
+
+          describe('social profile data', () => {
+            it('displays JSON', () => {
+              cy.get('.react-json-view').its('length').should('eq', 1);
+              cy.get('.react-json-view').contains('locale');
+              cy.get('.react-json-view').contains('picture');
+              cy.get('.react-json-view').contains('user_id');
+              cy.get('.react-json-view').contains('name');
+            });
           });
         });
       });
@@ -115,21 +137,31 @@ context('root/Agent show', function() {
           cy.login(_profile.email, _profile);
           cy.get('#app-menu-button').click();
           cy.get('#admin-switch').should('not.be.checked');
-          cy.visit(`/#/agent/${memberAgent.id}`);
-          cy.wait(500);
+          cy.visit(`/#/agent/${memberAgent.socialProfile.id}`);
+          cy.wait(200);
         });
 
         it('lands in the right spot', () => {
-          cy.url().should('contain', `/#/agent/${memberAgent.id}`);
+          cy.url().should('contain', `/#/agent/${memberAgent.socialProfile.id}`);
         });
 
-        it('displays agent social profile info in form', function() {
+        it('displays agent\'s editable info in form', function() {
           cy.get('h3').contains('Profile');
           cy.get('input[name="name"][type="text"]').should('have.value', memberAgent.name);
           cy.get('input[name="name"][type="text"]').should('be.disabled');
           cy.get('input[name="email"][type="email"]').should('have.value', memberAgent.email);
           cy.get('input[name="email"][type="email"]').should('be.disabled');
           cy.get('button[type="submit"]').should('not.exist');
+        });
+
+        describe('social profile data', () => {
+          it('displays JSON', () => {
+            cy.get('.react-json-view').its('length').should('eq', 1);
+            cy.get('.react-json-view').contains('locale');
+            cy.get('.react-json-view').contains('picture');
+            cy.get('.react-json-view').contains('user_id');
+            cy.get('.react-json-view').contains('name');
+          });
         });
       });
 
@@ -143,22 +175,32 @@ context('root/Agent show', function() {
           cy.get('#agent-button').contains('Profile').click();
           cy.task('query', `SELECT * FROM "Agents" WHERE "email"='${_profile.email}' LIMIT 1;`).then(([results, metadata]) => {
             root = results[0];
-            cy.visit(`/#/agent/${root.id}`);
+            cy.visit(`/#/agent/${root.socialProfile.id}`);
             cy.wait(500);
           });
         });
 
         it('lands in the right spot', () => {
-          cy.url().should('contain', `/#/agent/${root.id}`);
+          cy.url().should('contain', `/#/agent/${root.socialProfile.id}`);
         });
 
-        it('displays agent social profile info in form', () => {
+        it('displays agent\'s editable info in form', () => {
           cy.get('h3').contains('Profile');
           cy.get('input[name="name"][type="text"]').should('have.value', root.name);
           cy.get('input[name="name"][type="text"]').should('not.be.disabled');
           cy.get('input[name="email"][type="email"]').should('have.value', root.email);
           cy.get('input[name="email"][type="email"]').should('be.disabled');
           cy.get('button[type="submit"]').should('exist');
+        });
+
+        describe('social profile data', () => {
+          it('displays JSON', () => {
+            cy.get('.react-json-view').its('length').should('eq', 1);
+            cy.get('.react-json-view').contains('locale');
+            cy.get('.react-json-view').contains('picture');
+            cy.get('.react-json-view').contains('user_id');
+            cy.get('.react-json-view').contains('name');
+          });
         });
       });
     });
