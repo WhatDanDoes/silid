fb6d0dccd26dde708ba396f9d79c66e5f3064a5a
Daniel Bidulock <daniel_bidulock@sil.org>
2020-04-23 12:36:35 -0600

    Comment out unused code to squelch build server warnings

--- src/identity-react/src/routes/TeamInfo.jsx
+++ src/identity-react/src/routes/TeamInfo.jsx
@@ -1,33 +1,32 @@
 import React, { useState, useEffect } from 'react';
-import { Redirect } from 'react-router-dom';
-import TextField from '@material-ui/core/TextField';
-import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+//import { Redirect } from 'react-router-dom';
+//import TextField from '@material-ui/core/TextField';
+import { createStyles, makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-import Fab from '@material-ui/core/Fab';
-import PersonAddIcon from '@material-ui/icons/PersonAdd';
-import Button from '@material-ui/core/Button';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import DeleteForeverOutlinedIcon from '@material-ui/icons/DeleteForeverOutlined';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
+//import Card from '@material-ui/core/Card';
+//import CardContent from '@material-ui/core/CardContent';
+//import Fab from '@material-ui/core/Fab';
+//import PersonAddIcon from '@material-ui/icons/PersonAdd';
+//import Button from '@material-ui/core/Button';
+//import List from '@material-ui/core/List';
+//import ListItem from '@material-ui/core/ListItem';
+//import ListItemIcon from '@material-ui/core/ListItemIcon';
+//import ListItemText from '@material-ui/core/ListItemText';
+//import DeleteForeverOutlinedIcon from '@material-ui/icons/DeleteForeverOutlined';
+//import InboxIcon from '@material-ui/icons/MoveToInbox';
 
 import Grid from '@material-ui/core/Grid';
 
-import { Team } from '../types/Team';
 import Flash from '../components/Flash';
 
-import { useAuthState } from '../auth/Auth';
-import { useAdminState } from '../auth/Admin';
+//import { useAuthState } from '../auth/Auth';
+//import { useAdminState } from '../auth/Admin';
 
 import useGetTeamInfoService from '../services/useGetTeamInfoService';
-import usePutTeamService from '../services/usePutTeamService';
-import useDeleteTeamService from '../services/useDeleteTeamService';
-import usePutTeamMemberService from '../services/usePutTeamMemberService';
-import useDeleteTeamMemberService from '../services/useDeleteTeamMemberService';
+//import usePutTeamService from '../services/usePutTeamService';
+//import useDeleteTeamService from '../services/useDeleteTeamService';
+//import usePutTeamMemberService from '../services/usePutTeamMemberService';
+//import useDeleteTeamMemberService from '../services/useDeleteTeamMemberService';
 
 /**
  * For profile data display
@@ -44,7 +43,7 @@ import Link from '@material-ui/core/Link';
 
 
 
-const useStyles = makeStyles((theme: Theme) =>
+const useStyles = makeStyles((theme) =>
   createStyles({
     margin: {
       margin: theme.spacing(1),
@@ -70,29 +69,27 @@ const useStyles = makeStyles((theme: Theme) =>
   }),
 );
 
-export interface PrevFormState {
-  [key:string]: any
-}
 
-const TeamInfo = (props: any) => {
+const TeamInfo = (props) => {
   const classes = useStyles();
 
-  const {agent} = useAuthState();
-  const admin = useAdminState();
+//  const {agent} = useAuthState();
+//  const admin = useAdminState();
 
-  const [editFormVisible, setEditFormVisible] = useState(false);
-  const [agentFormVisible, setAgentFormVisible] = useState(false);
-  const [prevFormState, setPrevFormState] = useState<PrevFormState>({});
-  const [toOrganization, setToOrganization] = useState(false);
-  const [flashProps, setFlashProps] = useState({} as any);
+//  const [editFormVisible, setEditFormVisible] = useState(false);
+//  const [agentFormVisible, setAgentFormVisible] = useState(false);
+//  const [prevFormState, setPrevFormState] = useState({});
+//  const [toOrganization, setToOrganization] = useState(false);
+  //const [flashProps, setFlashProps] = useState({});
+  const [flashProps] = useState({});
 
-  const [teamInfo, setTeamInfo] = useState<Team>({} as Team);
+  const [teamInfo, setTeamInfo] = useState({});
 
   const service = useGetTeamInfoService(props.match.params.id);
-  let { publishTeam } = usePutTeamService();
-  let { deleteTeam } = useDeleteTeamService();
-  let { putTeamMember } = usePutTeamMemberService();
-  let { deleteTeamMember } = useDeleteTeamMemberService(props.match.params.id);
+//  let { publishTeam } = usePutTeamService();
+//  let { deleteTeam } = useDeleteTeamService();
+//  let { putTeamMember } = usePutTeamMemberService();
+//  let { deleteTeamMember } = useDeleteTeamMemberService(props.match.params.id);
 
   useEffect(() => {
     if (service.status === 'loaded') {
@@ -103,112 +100,112 @@ const TeamInfo = (props: any) => {
   /**
    * Update this team
    */
-  const handleSubmit = (evt:any) => {
-    evt.preventDefault();
-
-    const formData = new FormData(evt.target);
-    let data = {} as Team;
-    for (const [key, value] of formData.entries()) {
-      data[key] = value;
-    }
-
-    publishTeam(data).then(results => {
-      setEditFormVisible(false);
-      setTeamInfo({ results, ...teamInfo } as Team);
-    }).catch(err => {
-      console.log(err);
-    });
-  }
+//  const handleSubmit = (evt) => {
+//    evt.preventDefault();
+//
+//    const formData = new FormData(evt.target);
+//    let data = {};
+//    for (const [key, value] of formData.entries()) {
+//      data[key] = value;
+//    }
+//
+//    publishTeam(data).then(results => {
+////      setEditFormVisible(false);
+//      setTeamInfo({ results, ...teamInfo });
+//    }).catch(err => {
+//      console.log(err);
+//    });
+//  }
 
   /**
    * Remove this team
    */
-  const handleDelete = (evt:any) => {
-    if (teamInfo.members.length > 1) {
-      return window.alert('Remove all team members before deleting the team');
-    }
-    if (window.confirm('Delete team?')) {
-      deleteTeam(teamInfo.id).then(results => {
-        setToOrganization(true);
-      });
-    }
-  }
+//  const handleDelete = (evt) => {
+//    if (teamInfo.members.length > 1) {
+//      return window.alert('Remove all team members before deleting the team');
+//    }
+//    if (window.confirm('Delete team?')) {
+//      deleteTeam(teamInfo.id).then(results => {
+//        setToOrganization(true);
+//      });
+//    }
+//  }
 
   /**
    * Set tool-tip message on field validation
    */
-  const customMessage = (evt:React.ChangeEvent<HTMLInputElement>) => {
-    evt.target.setCustomValidity(`${evt.target.name} required`);
-  }
+//  const customMessage = (evt) => {
+//    evt.target.setCustomValidity(`${evt.target.name} required`);
+//  }
 
   /**
    * Keep track of team form state
    *
    * This needs to be replaced, as it does in OrganizationInfo...
    */
-  const onChange = (evt:React.ChangeEvent<HTMLInputElement>) => {
-    if (!prevFormState[evt.target.name]) {
-      const s = { ...prevFormState};
-      s[evt.target.name] = teamInfo[evt.target.name];
-      setPrevFormState(s);
-    }
-    const f = { ...teamInfo };
-    f[evt.target.name] = evt.target.value.trimLeft();
-    setTeamInfo(f);
-  }
+//  const onChange = (evt) => {
+//    if (!prevFormState[evt.target.name]) {
+//      const s = { ...prevFormState};
+//      s[evt.target.name] = teamInfo[evt.target.name];
+//      setPrevFormState(s);
+//    }
+//    const f = { ...teamInfo };
+//    f[evt.target.name] = evt.target.value.trimLeft();
+//    setTeamInfo(f);
+//  }
 
   /**
    * Redirect to `/organization` when this team is deleted
    */
-  if (toOrganization) {
-    return <Redirect to={{ pathname: `/organization/${teamInfo.organizationId}`, state: 'Team deleted' }} />
-  }
+//  if (toOrganization) {
+//    return <Redirect to={{ pathname: `/organization/${teamInfo.organizationId}`, state: 'Team deleted' }} />
+//  }
 
   /**
    * Add a new member to this team
    */
-  const handleMembershipChange = (evt:any) => {
-    evt.preventDefault();
-    const formData = new FormData(evt.target);
-
-    let data = {} as any;
-    for (const [key, value] of formData.entries()) {
-      data[key] = value;
-    }
-
-    putTeamMember(data).then((results: any) => {
-      setAgentFormVisible(false);
-      if (results.message) {
-        setFlashProps({ message: results.message, variant: 'warning' });
-      }
-      else {
-        teamInfo.members.push(results);
-        setTeamInfo({ ...teamInfo } as Team);
-      }
-    }).catch(err => {
-      console.log(err);
-    });
-  }
+//  const handleMembershipChange = (evt) => {
+//    evt.preventDefault();
+//    const formData = new FormData(evt.target);
+//
+//    let data = {};
+//    for (const [key, value] of formData.entries()) {
+//      data[key] = value;
+//    }
+//
+//    putTeamMember(data).then((results) => {
+//      setAgentFormVisible(false);
+//      if (results.message) {
+//        setFlashProps({ message: results.message, variant: 'warning' });
+//      }
+//      else {
+//        teamInfo.members.push(results);
+//        setTeamInfo({ ...teamInfo });
+//      }
+//    }).catch(err => {
+//      console.log(err);
+//    });
+//  }
 
   /**
    * Remove member from team
    */
-  const handleMemberDelete = (memberId: any) => {
-    if (window.confirm('Remove member?')) {
-      deleteTeamMember(memberId).then(results => {
-        const index = teamInfo.members.findIndex(member => member.id === memberId);
-        teamInfo.members.splice(index, 1);
-        setTeamInfo({ ...teamInfo } as Team);
-        setFlashProps({ message: 'Member removed', variant: 'success' });
-      }).catch(err => {
-        console.log(err);
-      });
-    }
-  }
+//  const handleMemberDelete = (memberId) => {
+//    if (window.confirm('Remove member?')) {
+//      deleteTeamMember(memberId).then(results => {
+//        const index = teamInfo.members.findIndex(member => member.id === memberId);
+//        teamInfo.members.splice(index, 1);
+//        setTeamInfo({ ...teamInfo });
+//        setFlashProps({ message: 'Member removed', variant: 'success' });
+//      }).catch(err => {
+//        console.log(err);
+//      });
+//    }
+//  }
 
-  function ListItemLink(props:any) {
-    return <ListItem className='list-item' button component="a" {...props} />;
-  }
+//  function ListItemLink(props) {
+//    return <ListItem className='list-item' button component="a" {...props} />;
+//  }
 
   return (
     <div className={classes.root}>
