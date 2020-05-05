5c851bac2a8dcdfcbb4f9b9c9049422c79196a20
Daniel Bidulock <daniel_bidulock@sil.org>
2020-05-05 13:39:31 -0600

    Hello, stubUserAppMetadataRead

---
+++ src/silid-server/spec/support/auth0Endpoints/stubUserAppMetadataRead.js
@@ -0,0 +1,45 @@
+const nock = require('nock');
+const querystring = require('querystring');
+const apiScope = require('../../../config/apiPermissions');
+const jwt = require('jsonwebtoken');
+const stubOauthToken =  require('./stubOauthToken');
+
+const _profile = require('../../fixtures/sample-auth0-profile-response');
+
+/**
+ * 2020-5-5
+ *
+ * I'm pretty sure this stub doesn't reflect reality. It satisifies
+ * the tests to retrieve a list of a agents's teams, but I'm not
+ * 100% sure this will even ever happen in real life.
+ *
+ *
+ * This stubs the Auth0 endpoint that reads a user and his metadata
+ *
+ * @param array
+ * @param function
+ */
+module.exports = function(done) {
+
+  require('../setupKeystore').then(singleton => {
+    let { pub, prv, keystore } = singleton.keyStuff;
+
+    stubOauthToken([apiScope.read.users, apiScope.read.usersAppMetadata], (err, oauthScopes) => {
+      if (err) return done(err);
+
+      ({accessToken, oauthTokenScope} = oauthScopes);
+
+      /**
+       * GET `/users/:id`. Get a single user by Auth0 ID
+       */
+      const userAppMetadataReadScope = nock(`https://${process.env.AUTH0_DOMAIN}`, { reqheaders: { authorization: `Bearer ${accessToken}`} })
+        .log(console.log)
+        .get(/api\/v2\/users\/.+/)
+        .query({})
+        .reply(200, _profile);
+
+        done(null, {userAppMetadataReadScope, oauthTokenScope});
+
+    });
+  });
+};
