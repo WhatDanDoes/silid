4be68e0710b6034a616c12003c01edd5ef42e48d
Daniel Bidulock <daniel_bidulock@sil.org>
2020-07-08 08:30:02 -0600

    Clean up

--- src/identity-react/cypress/integration/organizer/organizationDeleteSpec.js
+++ src/identity-react/cypress/integration/organizer/organizationDeleteSpec.js
@@ -182,85 +182,85 @@ context('organizer/Organization delete', function() {
         });
       });
 
-////      describe('on team edit page', () => {
-////        context('when team has team members', () => {
-////          beforeEach(function() {
-////            cy.request({ url: `/team/${team.id}/agent`, method: 'PUT', body: { email: anotherAgent.email } }).then((res) => {
-////              cy.visit('/#/').then(() => {
-////                cy.get('#app-menu-button').click();
-////                cy.get('#organization-button').click();
-////                cy.contains('One Book Canada').click();
-////                cy.contains(team.name).click();
-////                cy.get('button#edit-team').click();
-////              });
-////            });
-////          });
-////
-////          it('does not allow deletion', function(done) {
-////            cy.on('window:alert', (str) => {
-////              expect(str).to.eq('Remove all team members before deleting the team');
-////              done();
-////            });
-////            cy.get('button#delete-team').click();
-////          });
-////        });
-////
-////        context('when team has no team members', () => {
-////          beforeEach(() => {
-////            cy.task('query', `SELECT * FROM "TeamMembers";`).then(([results, metadata]) => {
-////              expect(results.length).to.eq(1);
-////              expect(results[0].AgentId).to.eq(agent.id);
-////              cy.visit('/#/').then(() => {
-////                cy.get('#app-menu-button').click();
-////                cy.get('#organization-button').click();
-////                cy.contains('One Book Canada').click();
-////                cy.contains(team.name).click();
-////                cy.get('button#edit-team').click();
-////              });
-////            });
-////          });
-////
-////          it('displays a popup warning', function(done) {
-////            cy.on('window:confirm', (str) => {
-////              expect(str).to.eq('Delete team?');
-////              done();
-////            });
-////            cy.get('button#delete-team').click();
-////          });
-////
-////          it('lands in the proper place', () => {
-////            cy.on('window:confirm', (str) => {
-////              return true;
-////            });
-////            cy.get('button#delete-team').click();
-////            cy.url().should('contain', `/#/organization/${organization.id}`);
-////          });
-////
-////          it('removes record from the database', () => {
-////            cy.on('window:confirm', (str) => {
-////              return true;
-////            });
-////            cy.task('query', `SELECT * FROM "Teams";`).then(([results, metadata]) => {
-////              expect(results.length).to.eq(1);
-////              cy.get('button#delete-team').click();
-////              cy.wait(500);
-////              cy.task('query', `SELECT * FROM "Teams";`).then(([results, metadata]) => {
-////                expect(results.length).to.eq(0);
-////              });
-////            });
-////          });
-////
-////          it('renders the interface correctly on completion with success message', () => {
-////            cy.on('window:confirm', (str) => {
-////              return true;
-////            });
-////            cy.get('button#delete-team').click();
-////            cy.wait(500);
-////            cy.get('#organization-team-list').should('not.exist');
-////            cy.contains('Team deleted');
-////          });
-////        });
-////      });
+//      describe('on team edit page', () => {
+//        context('when team has team members', () => {
+//          beforeEach(function() {
+//            cy.request({ url: `/team/${team.id}/agent`, method: 'PUT', body: { email: anotherAgent.email } }).then((res) => {
+//              cy.visit('/#/').then(() => {
+//                cy.get('#app-menu-button').click();
+//                cy.get('#organization-button').click();
+//                cy.contains('One Book Canada').click();
+//                cy.contains(team.name).click();
+//                cy.get('button#edit-team').click();
+//              });
+//            });
+//          });
+//
+//          it('does not allow deletion', function(done) {
+//            cy.on('window:alert', (str) => {
+//              expect(str).to.eq('Remove all team members before deleting the team');
+//              done();
+//            });
+//            cy.get('button#delete-team').click();
+//          });
+//        });
+//
+//        context('when team has no team members', () => {
+//          beforeEach(() => {
+//            cy.task('query', `SELECT * FROM "TeamMembers";`).then(([results, metadata]) => {
+//              expect(results.length).to.eq(1);
+//              expect(results[0].AgentId).to.eq(agent.id);
+//              cy.visit('/#/').then(() => {
+//                cy.get('#app-menu-button').click();
+//                cy.get('#organization-button').click();
+//                cy.contains('One Book Canada').click();
+//                cy.contains(team.name).click();
+//                cy.get('button#edit-team').click();
+//              });
+//            });
+//          });
+//
+//          it('displays a popup warning', function(done) {
+//            cy.on('window:confirm', (str) => {
+//              expect(str).to.eq('Delete team?');
+//              done();
+//            });
+//            cy.get('button#delete-team').click();
+//          });
+//
+//          it('lands in the proper place', () => {
+//            cy.on('window:confirm', (str) => {
+//              return true;
+//            });
+//            cy.get('button#delete-team').click();
+//            cy.url().should('contain', `/#/organization/${organization.id}`);
+//          });
+//
+//          it('removes record from the database', () => {
+//            cy.on('window:confirm', (str) => {
+//              return true;
+//            });
+//            cy.task('query', `SELECT * FROM "Teams";`).then(([results, metadata]) => {
+//              expect(results.length).to.eq(1);
+//              cy.get('button#delete-team').click();
+//              cy.wait(500);
+//              cy.task('query', `SELECT * FROM "Teams";`).then(([results, metadata]) => {
+//                expect(results.length).to.eq(0);
+//              });
+//            });
+//          });
+//
+//          it('renders the interface correctly on completion with success message', () => {
+//            cy.on('window:confirm', (str) => {
+//              return true;
+//            });
+//            cy.get('button#delete-team').click();
+//            cy.wait(500);
+//            cy.get('#organization-team-list').should('not.exist');
+//            cy.contains('Team deleted');
+//          });
+//        });
+//      });
     });
   });
 });
