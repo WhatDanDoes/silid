c6d57f012c3c1508bb794dc6ab9b48a6df74cd14
Daniel Bidulock <daniel_bidulock@sil.org>
2020-07-13 16:48:31 -0600

    Hello, 20200713220658-add-data-column-to-update-table

---
+++ src/silid-server/migrations/20200713220658-add-data-column-to-update-table.js
@@ -0,0 +1,33 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
+    */
+    return queryInterface.sequelize.transaction(t => {
+      return Promise.all([
+        queryInterface.addColumn('Updates', 'data', {
+          type: Sequelize.DataTypes.JSON,
+          allowNull: false,
+        }, { transaction: t }),
+        queryInterface.removeColumn('Updates', 'teamId', { transaction: t }),
+        queryInterface.removeColumn('Updates', 'name', { transaction: t }),
+      ]);
+    });
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.dropTable('users');
+    */
+  }
+};
