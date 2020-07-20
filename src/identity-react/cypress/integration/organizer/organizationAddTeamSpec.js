5a9508acb9141bcd9c8495330f907bfd365ff5b0
Daniel Bidulock <daniel_bidulock@sil.org>
2020-07-20 07:59:11 -0600

    Get team-add-to-org tests for when organizer is also team leader

--- src/identity-react/cypress/integration/organizer/organizationAddTeamSpec.js
+++ src/identity-react/cypress/integration/organizer/organizationAddTeamSpec.js
@@ -81,7 +81,7 @@ context('organizer/Organization add team', function() {
         cy.get('#team-profile-info tbody tr td input#team-name-field').should('be.disabled');
         cy.get('#team-profile-info tbody tr td').contains('coach@example.com');
         cy.get('#team-profile-info #add-team-to-organization').should('exist');
-        cy.get('#team-profile-info button#delete-team').should('not.exist');
+        cy.get('button#delete-team').should('not.exist');
         cy.get('#team-profile-info button#save-team').should('not.exist');
         cy.get('#team-profile-info button#cancel-team-changes').should('not.exist');
       });
@@ -178,7 +178,7 @@ context('organizer/Organization add team', function() {
       beforeEach(function() {
         _profile = { ...this.profile };
 
-        // A convenient way to create a new team leader agent
+        // Login team leader agent
         cy.login('coach@example.com', {..._profile, email: 'coach@example.com', name: 'Curt Malawsky'});
 
         cy.contains('The Calgary Roughnecks').click();
@@ -189,6 +189,7 @@ context('organizer/Organization add team', function() {
         cy.get('button[title="Save"]').click();
         cy.wait(300);
 
+        // Login organizer agent
         cy.login(_profile.email, _profile, rolePermissions.organizer);
         // Accept team invitaiton
         cy.get('#rsvps-table table tbody').find('tr').its('length').should('eq', 1);
@@ -213,7 +214,7 @@ context('organizer/Organization add team', function() {
         cy.get('#team-profile-info tbody tr td input#team-name-field').should('be.disabled');
         cy.get('#team-profile-info tbody tr td').contains('coach@example.com');
         cy.get('#team-profile-info #add-team-to-organization').should('exist');
-        cy.get('#team-profile-info button#delete-team').should('not.exist');
+        cy.get('button#delete-team').should('not.exist');
         cy.get('#team-profile-info button#save-team').should('not.exist');
         cy.get('#team-profile-info button#cancel-team-changes').should('not.exist');
       });
@@ -306,6 +307,123 @@ context('organizer/Organization add team', function() {
       });
     });
 
+    context('organizer is team leader', () => {
+      beforeEach(function() {
+        // Create a team
+        cy.contains('Identity').click();
+        cy.get('#teams-table button span span').contains('add_box').click();
+        cy.get('#teams-table table tbody tr td div div input[placeholder="Name"]').type('The Saskatchewan Rush');
+        cy.get('#teams-table table tbody tr td div button[title="Save"]').click();
+        cy.wait(300);
+        cy.contains('The Saskatchewan Rush').click();
+        cy.task('query', `SELECT * FROM "Agents" WHERE "email"='${_profile.email}' LIMIT 1;`).then(([results, metadata]) => {
+          organizerAgent = results[0];
+        });
+      });
+
+      it('lands in the right spot', () => {
+        cy.url().should('contain', `/#/team/${organizerAgent.socialProfile.user_metadata.teams[0].id}`);
+      });
+
+      it('displays the organizer-enabled interface', () => {
+        cy.get('#team-profile-info tbody').find('tr').its('length').should('eq', 3);
+        cy.get('#team-profile-info tbody tr td input#team-name-field').should('have.value', 'The Saskatchewan Rush');
+        cy.get('#team-profile-info tbody tr td input#team-name-field').should('not.be.disabled');
+        cy.get('#team-profile-info tbody tr td').contains(organizerAgent.email);
+        cy.get('#team-profile-info #add-team-to-organization').should('exist');
+        cy.get('button#delete-team').should('exist');
+        cy.get('#team-profile-info button#save-team').should('not.exist');
+        cy.get('#team-profile-info button#cancel-team-changes').should('not.exist');
+      });
+
+      describe('#add-team-to-organization', () => {
+        it('reveals organizer\'s organization list', () => {
+          cy.get('#organizations-table').should('not.exist');
+          cy.get('#team-profile-info #add-team-to-organization').click();
+          cy.get('#organizations-table').should('exist');
+        });
+
+        it('hides itself and reveals button to close organization menu', () => {
+          cy.get('#team-profile-info #close-team-organization-menu').should('not.exist');
+          cy.get('#team-profile-info #add-team-to-organization').click();
+          cy.get('#team-profile-info #add-team-to-organization').should('not.exist');
+          cy.get('#team-profile-info #close-team-organization-menu').should('exist');
+        });
+
+        describe('#organizations-table', () => {
+          beforeEach(() => {
+            cy.get('#team-profile-info #add-team-to-organization').click();
+          });
+
+          it('displays a list of the organizer agent\'s organizations', () => {
+            cy.get('#organizations-table h6').contains('Your Organizations');
+            cy.get('#organizations-table table tbody tr td').contains('No records to display').should('not.exist');
+
+            cy.get('#organizations-table table thead tr th').contains('Actions');
+            cy.get('#organizations-table table tbody tr td button').contains('add');
+            cy.get('#organizations-table table thead tr th').contains('Name');
+            cy.get('#organizations-table table tbody tr td').contains(organizerAgent.socialProfile.user_metadata.organizations[0].name);
+            cy.get('#organizations-table table tbody tr td a').should('contain', organizerAgent.socialProfile.user_metadata.organizations[0].name).
+              and('have.attr', 'href').and('equal', `#organization/${organizerAgent.socialProfile.user_metadata.organizations[0].id}`);
+          });
+
+          describe('add to organization button', () => {
+            it('updates the interface', () => {
+              cy.get('#team-profile-info tbody tr:last-of-type th').contains('Organization:');
+              cy.get('#team-profile-info tbody tr:last-of-type td #close-team-organization-menu').should('exist');
+
+              cy.get('#organizations-table table tbody tr td button').contains('add').click();
+              cy.wait(300);
+
+              cy.get('#team-profile-info tbody tr:last-of-type td a').should('contain', organizerAgent.socialProfile.user_metadata.organizations[0].name).
+                and('have.attr', 'href').and('equal', `#organization/${organizerAgent.socialProfile.user_metadata.organizations[0].id}`);
+
+              cy.get('#team-profile-info tbody tr:last-of-type td:last-of-type #close-team-organization-menu').should('not.exist');
+              cy.get('#team-profile-info tbody tr:last-of-type td:last-of-type #remove-team-from-organization').should('exist');
+            });
+
+            it('hides the #organizations-table', () => {
+              cy.get('#organizations-table').should('exist');
+              cy.get('#organizations-table table tbody tr td button').contains('add').click();
+              cy.wait(300);
+              cy.get('#organizations-table').should('not.exist');
+            });
+
+            it('lands in the right spot', () => {
+              cy.get('#organizations-table table tbody tr td button').contains('add').click();
+              cy.wait(300);
+              cy.url().should('contain', `/#/team/${organizerAgent.socialProfile.user_metadata.teams[0].id}`);
+            });
+
+            it('displays a friendly message', () => {
+              cy.get('#organizations-table table tbody tr td button').contains('add').click();
+              cy.wait(300);
+              cy.get('#flash-message').contains('The Saskatchewan Rush are now part of The National Lacrosse League');
+            });
+          });
+        });
+
+        describe('#close-team-organization-menu', () => {
+          beforeEach(() => {
+            cy.get('#team-profile-info #add-team-to-organization').click();
+          });
+
+          it('hides the organizer\'s organization list', () => {
+            cy.get('#organizations-table').should('exist');
+            cy.get('#team-profile-info #close-team-organization-menu').click();
+            cy.get('#organizations-table').should('not.exist');
+          });
+
+          it('hides itself and reveals button to open organization menu', () => {
+            cy.get('#team-profile-info #add-team-to-organization').should('not.exist');
+            cy.get('#team-profile-info #close-team-organization-menu').click();
+            cy.get('#team-profile-info #add-team-to-organization').should('exist');
+            cy.get('#team-profile-info #close-team-organization-menu').should('not.exist');
+          });
+        });
+      });
+    });
+
     context('team is member of another organization', () => {
       let anotherOrganizerAgent;
       beforeEach(() => {
@@ -349,7 +467,7 @@ context('organizer/Organization add team', function() {
         cy.get('#team-profile-info tbody tr td input#team-name-field').should('be.disabled');
         cy.get('#team-profile-info tbody tr td').contains('coach@example.com');
         cy.get('#team-profile-info #add-team-to-organization').should('not.exist');
-        cy.get('#team-profile-info button#delete-team').should('not.exist');
+        cy.get('button#delete-team').should('not.exist');
         cy.get('#team-profile-info button#save-team').should('not.exist');
         cy.get('#team-profile-info button#cancel-team-changes').should('not.exist');
         cy.get('#team-profile-info tbody tr:last-of-type td a').should('contain', organizerAgent.socialProfile.user_metadata.organizations[0].name).
