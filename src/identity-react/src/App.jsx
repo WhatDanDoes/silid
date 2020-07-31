911fd11aaea9681b4f94e414d7528abd2776c56c
Daniel Bidulock <daniel_bidulock@sil.org>
2020-07-31 08:41:33 -0600

    Clean up

--- src/identity-react/src/App.jsx
+++ src/identity-react/src/App.jsx
@@ -13,50 +13,6 @@ import { AuthProvider } from './auth/Auth';
 import { AdminProvider } from './auth/Admin';
 import PrivateRoute from './components/PrivateRoute';
 import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
-import { IntlProvider, FormattedMessage } from "react-intl";
-
-/**
- * Localization
- */
-const messages = {
-  eng: {
-    profileTable: {
-      header: 'Profile',
-      email: 'Email',
-      providerLocale: 'Provider Locale',
-      silLocale: 'SIL Locale',
-      Roles: 'Roles',
-    },
-    teamsTable: {
-      header: 'Teams',
-      name: 'Name',
-      leader: 'Leader',
-      empty: 'No records to display',
-    },
-    socialData: {
-      header: 'Social Data',
-    }
-  },
-  tlh: {
-    profileTable: {
-      header: 'tlhIlHal De\'',
-      email: 'De\'wI\' QIn',
-      providerLocale: 'Latlh Hol',
-      silLocale: 'SIL Hol',
-      Roles: 'Naw\'',
-    },
-    teamsTable: {
-      header: 'Ghom',
-      name: 'Pong',
-      leader: 'DevwI\'',
-      empty: 'Pagh ta',
-    },
-    socialData: {
-      header: 'BoS De\'',
-    }
-
-  },
-};
 
 /**
  * Colour everything SIL blue
@@ -76,27 +32,25 @@ function App() {
     <div className="App">
       <AuthProvider>
         <AdminProvider>
-          <IntlProvider locale='eng' messages={messages['eng']}>
-            <ThemeProvider theme={theme}>
-              <HashRouter>
-                <Route
-                  path="/"
-                  render={props => <Home message={message} {...props} />}
-                />
-                <Switch>
-                  <PrivateRoute path="/agent/admin" component={AgentDirectory} redirect="/" />
-                  <PrivateRoute path="/agent/:id" component={Agent} redirect="/" />
-                  <PrivateRoute path="/agent" component={Agent} redirect="/" />
-                  <PrivateRoute path="/organization/admin" component={Organization} redirect="/" />
-                  <PrivateRoute path="/organization/:id" component={OrganizationInfo} redirect="/" />
-                  <PrivateRoute path="/organization" component={Organization} redirect="/" />
-                  <PrivateRoute path="/team/admin" component={Team} redirect="/" />
-                  <PrivateRoute path="/team/:id" component={TeamInfo} redirect="/" />
-                  <PrivateRoute path="/team" component={Team} redirect="/" />
-                </Switch>
-              </HashRouter>
-            </ThemeProvider>
-          </IntlProvider>
+          <ThemeProvider theme={theme}>
+            <HashRouter>
+              <Route
+                path="/"
+                render={props => <Home message={message} {...props} />}
+              />
+              <Switch>
+                <PrivateRoute path="/agent/admin" component={AgentDirectory} redirect="/" />
+                <PrivateRoute path="/agent/:id" component={Agent} redirect="/" />
+                <PrivateRoute path="/agent" component={Agent} redirect="/" />
+                <PrivateRoute path="/organization/admin" component={Organization} redirect="/" />
+                <PrivateRoute path="/organization/:id" component={OrganizationInfo} redirect="/" />
+                <PrivateRoute path="/organization" component={Organization} redirect="/" />
+                <PrivateRoute path="/team/admin" component={Team} redirect="/" />
+                <PrivateRoute path="/team/:id" component={TeamInfo} redirect="/" />
+                <PrivateRoute path="/team" component={Team} redirect="/" />
+              </Switch>
+            </HashRouter>
+          </ThemeProvider>
         </AdminProvider>
       </AuthProvider>
     </div>
