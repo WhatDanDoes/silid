9e482f2fe3f1db27f77590e6d5ecf2cb1bb37014
Daniel Bidulock <daniel_bidulock@sil.org>
2021-03-01 14:52:44 -0700

    Consider react test build when setting up routing

--- src/silid-server/app.js
+++ src/silid-server/app.js
@@ -94,7 +94,8 @@ if (process.env.NODE_ENV === 'e2e') {
 
 if (
   process.env.NODE_ENV === 'production' ||
-  process.env.NODE_ENV === 'staging'
+  process.env.NODE_ENV === 'staging' ||
+  process.env.TEST_BUILD
 ) {
   app.use(express.static(path.join(__dirname, 'build')));
   app.use(express.static(path.join(__dirname, 'public')));
