b0bba76fb5f741f63e6b18c7ae232d4c1422b255
Daniel Bidulock <daniel_bidulock@sil.org>
2021-03-17 12:54:37 -0600

    Remove Typescript pre-processor clutter

--- src/identity-react/cypress/plugins/index.js
+++ src/identity-react/cypress/plugins/index.js
@@ -1,9 +1,7 @@
 require('dotenv').config();
-const cypressTypeScriptPreprocessor = require('./cy-ts-preprocessor')
 const db = require('./database');
 
 module.exports = (on, config) => {
-  on('file:preprocessor', cypressTypeScriptPreprocessor);
 
   /**
    * 2019-11-25 https://stackoverflow.com/questions/52070262/cypress-pipe-console-log-and-command-log-to-output
