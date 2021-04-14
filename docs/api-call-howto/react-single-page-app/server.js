94ad35a20d8363a6aff5c92d87b69789a323e6f6
Daniel Bidulock <daniel@capitolhill.ca>
2021-04-14 18:18:29 +0000

    Accommodate client-side routing in production

--- docs/api-call-howto/react-single-page-app/server.js
+++ docs/api-call-howto/react-single-page-app/server.js
@@ -11,4 +11,15 @@ app.use(morgan("dev"));
 app.use(helmet());
 app.use(express.static(join(__dirname, "build")));
 
+/**
+ * 2021-4-14
+ *
+ * Client-side routing in production
+ *
+ * https://create-react-app.dev/docs/deployment/#serving-apps-with-client-side-routing
+ */
+app.get('/*', function (req, res) {
+  res.sendFile(join(__dirname, 'build', 'index.html'));
+});
+
 app.listen(port, () => console.log(`Server listening on port ${port}`));
